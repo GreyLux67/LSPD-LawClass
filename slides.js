5627e95aefd455c4eb06f41aa56b6301f009bb3e
GreyLux67 <dominio2278salas@gmail.com>
2026-07-27 16:08:26 -0600

    Update slides.js

--- slides.js
+++ slides.js
@@ -62,9 +62,9 @@ const slides = [
 
         title: "Burdens of Proof",
 
-        subtitle: "The amount of evidence required to secure a conviction or judgment",
+        subtitle: "",
 
-        body: "",
+        body: "Levels of Burdens of Proof\n\n<strong>I. Reasonable Suspicion</strong>\n<strong>II. Probable Cause</strong>\n<strong>III. Preponderance of Evidence - Civil Suit</strong>\nThe applicant must show a preponderance of evidence which is; that it is more likely than not that the plaintiff or applicant proved their narrative.\n<strong>IV. Beyond a Reasonable Doubt - Criminal Case</strong>\nAll charges and certain appeals escalated to court must be prepared to demonstrate that their evidence for conviction is beyond a reasonable doubt, which is the highest standard of evidence. The prosecution must be able to articulate that there is no other reasonable explanation that can come from the evidence presented at trial, and the Court must be virtually certain of the defendant's guilt in order to render a guilty verdict.",
 
         bullets: [
 
@@ -79,14 +79,14 @@ const slides = [
         image: ""
     },
 
-    {
+ {
         eyebrow: "Module 1 Legal Information",
 
-        title: "Burdens of Proof",
+        title: "General Terms",
 
         subtitle: "",
 
-        body: "Levels of Burdens of Proof\n\n<strong>I. Reasonable Suspicion</strong>\n<strong>II. Probable Cause</strong>\n<strong>III. Preponderance of Evidence - Civil Suit</strong>\nThe applicant must show a preponderance of evidence which is; that it is more likely than not that the plaintiff or applicant proved their narrative.\n<strong>IV. Beyond a Reasonable Doubt - Criminal Case</strong>\nAll charges and certain appeals escalated to court must be prepared to demonstrate that their evidence for conviction is beyond a reasonable doubt, which is the highest standard of evidence. The prosecution must be able to articulate that there is no other reasonable explanation that can come from the evidence presented at trial, and the Court must be virtually certain of the defendant's guilt in order to render a guilty verdict.",
+        body: "<strong>Double Jeoprady</strong>\nProhibits anyone from being prosecuted twice for substantially the same crime\n<strong>Due Process</strong>\nThe guarantee that a defendant will receive a fair and impartial trial and process of the law. No one shall be deprived of life, liberty, or property without the due process of law.",
 
         bullets: [
 
