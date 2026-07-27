9161d413a0d51a1e1932a499482b49546355b375
GreyLux67 <dominio2278salas@gmail.com>
2026-07-27 12:31:53 -0600

    Update slides.js

--- slides.js
+++ slides.js
@@ -1,1 +1,13 @@
-
+const slides = [
+    {
+        eyebrow: "LOS SANTOS POLICE DEPARTMENT",
+        title: "Cadet Academy",
+        subtitle: "Legal Training Program",
+        bullets: [
+            "Constitutional Law",
+            "Search and Seizure",
+            "Traffic Stops",
+            "Miranda Rights"
+        ]
+    }
+];
